ef8e6a5a3cb96db6c33e68593a37118ee825d6c5
Eilo <eilo2518@gmail.com>
2011-10-28 17:13:48 -0500

    Cuentas VIP un typo. fix compile

--- src/server/game/Miscellaneous/Formulas.h
+++ src/server/game/Miscellaneous/Formulas.h
@@ -180,7 +180,7 @@ namespace Trinity
 
                 float premium_rate = pl->GetSession()->IsPremium() ? sWorld->getRate(RATE_XP_KILL_PREMIUM) : 1.0f;
 
-                return uint32(gain*sWorld->getRate(RATE_XP_KILL)*premium_rate)
+                return uint32(gain*sWorld->getRate(RATE_XP_KILL)*premium_rate);
             }
 
             sScriptMgr->OnGainCalculation(gain, pl, u);
