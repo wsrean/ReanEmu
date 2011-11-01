f48e2b7c3ea14bd587eac77e22069332edaa3951
Eilo <eilo2518@gmail.com>
2011-10-31 23:59:01 -0500

    ValianceWxpedition & WarsongOffensive: Reparacion de trituradoras - Machaca a la alianza

--- src/server/scripts/Northrend/grizzly_hills.cpp
+++ src/server/scripts/Northrend/grizzly_hills.cpp
@@ -1012,6 +1012,75 @@ public:
  }
 };
 
+/*#################################
+# npc_brokendown_shredders (27354)
+##################################*/
+
+class npc_brokendown_shredders : public CreatureScript
+{
+public:
+    npc_brokendown_shredders() : CreatureScript("npc_brokendown_shredders") { }
+
+    struct npc_brokendown_shreddersAI : public ScriptedAI
+    {
+        npc_brokendown_shreddersAI(Creature* creature) : ScriptedAI(creature) { }
+
+        void UpdateAI(const uint32 /*diff*/)
+        {
+            Unit* player = me->GetVehicleKit()->GetPassenger(0);
+                    if (!player && ((me->GetPositionX() != me->GetHomePosition().GetPositionX()) && (me->GetPositionY() != me->GetHomePosition().GetPositionY()) && (me->GetPositionZ() != me->GetHomePosition().GetPositionZ())))
+            {
+                me->DisappearAndDie();
+                me->Respawn();
+                return;
+            }
+        }
+    };
+
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_brokendown_shreddersAI(creature);
+    }
+};
+
+/*#################################
+# npc_shredders_taker (27371, 2742)
+##################################*/
+
+class npc_shredders_taker : public CreatureScript
+{
+public:
+    npc_shredders_taker() : CreatureScript("npc_shredders_taker") { }
+
+    struct npc_shredders_takerAI : public ScriptedAI
+    {
+        npc_shredders_takerAI(Creature* creature) : ScriptedAI(creature) { }
+
+        void MoveInLineOfSight(Unit* who)
+        {
+            if(!who)
+                return;
+
+            if (who->GetTypeId() != TYPEID_UNIT || who->GetEntry() != 27354 || !who->isCharmedOwnedByPlayerOrPlayer())
+                return;
+
+            Player* player = who->GetCharmerOrOwner()->ToPlayer();
+
+            if (player && me->IsWithinDistInMap(who, 15.0f))
+            {
+                player->KilledMonsterCredit(27396, 0);
+                player->ExitVehicle();
+                who->ToCreature()->DisappearAndDie();
+            }
+        }
+    };
+
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_shredders_takerAI(creature);
+    }
+};
+
 void AddSC_grizzly_hills()
 {
     new npc_orsonn_and_kodian;
@@ -1026,4 +1095,6 @@ void AddSC_grizzly_hills()
     new npc_lake_frog;
     new npc_maiden_of_ashwood_lake;
     new npc_maiden_of_drak_mar;
+    new npc_brokendown_shredders;
+    new npc_shredders_taker;
 }
