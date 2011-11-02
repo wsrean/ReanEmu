fd23e053c57e0bcba1f3e10b14fcc59c065edb1e
Eilo <eilo2518@gmail.com>
2011-11-02 16:26:48 -0500

    Eliminado Mensajes Anticheat del chat, presentados nuevamente en el centro de la pantalla, menos spam

--- sql/updates/world/2011_10_09_100_world_anticheat_mensajes.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `trinity_string` WHERE `entry` = 11002;
-INSERT INTO `trinity_string` (`entry`, `content_default`) VALUES
-(11003, '|cFFFFBF00[%s]:|cFFFFFFFF [%s] |cFF00FFFF detectado como posible cheater.');
--- src/server/game/Miscellaneous/Language.h
+++ src/server/game/Miscellaneous/Language.h
@@ -1080,7 +1080,6 @@ enum TrinityStrings
     LANG_AUTO_BROADCAST                 = 11000,
     LANG_INVALID_REALMID                = 11001,
     LANG_BAN_CHEATER                    = 11002,
-    LANG_CHEATER_CHATLOG                = 11003,
 
     // NOT RESERVED IDS                   12000-1999999999
     // `db_script_string` table index     2000000000-2000009999 (MIN_DB_SCRIPT_STRING_ID-MAX_DB_SCRIPT_STRING_ID)
