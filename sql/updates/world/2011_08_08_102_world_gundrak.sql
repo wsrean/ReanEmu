a4e8e51f85f67dab696d1e04c05371bc602452e8
Eilo <eilo2518@gmail.com>
2011-11-05 16:34:52 -0500

    Gundrak: Trigger en sql para una spell faltante

--- sql/updates/world/2011_08_08_102_world_gundrak.sql
+++ sql/updates/world/2011_08_08_102_world_gundrak.sql
@@ -55,4 +55,9 @@ INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (59453, 'spell_mojo_volley_trigger');
 UPDATE `creature_template` SET `unit_flags`=`unit_flags`&~512 WHERE `entry` IN (29307,31365);
 
+DELETE FROM `spell_script_names` WHERE `spell_id`=59452;
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(59452, 'spell_mojo_volley_targeting');
+
+
 
