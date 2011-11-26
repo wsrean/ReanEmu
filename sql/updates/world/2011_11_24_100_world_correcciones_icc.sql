16d88cd3e61913207416328cffd8a3ce645f8f34
Eilo <eilo2518@gmail.com>
2011-11-26 11:19:12 -0500

    ICC: Ajuste en velocidades de las babosas de putricidio y despawn de algunos puntos de spawn en la pelea de rotface que hacian aparecer los charcos en el aire

--- sql/updates/world/2011_11_24_100_world_correcciones_icc.sql
+++ sql/updates/world/2011_11_24_100_world_correcciones_icc.sql
@@ -1,15 +1,29 @@
 -- ICC trash mob
--- Corrigiendo arañas para que se las pueda pegar
+-- Corrigiendo posiciones de las arañas para que se las pueda pegar
 -- Nerub'ar Broodkeeper http://old.wowhead.com/npc=36725
 
 -- Las dos primeras del medio
-UPDATE `creature_template` SET `position_z` = 35.24 WHERE `guid`=201170;
-UPDATE `creature_template` SET `position_z` = 35.24 WHERE `guid`=201106;
-
+UPDATE `creature` SET `position_z` = 35.24 WHERE `guid`=201170;
+UPDATE `creature` SET `position_z` = 35.24 WHERE `guid`=201106;
 -- Las dos siguientes, osea arriba de las gradas
-UPDATE `creature_template` SET `position_z` = 44.57 WHERE `guid`=200949;
-UPDATE `creature_template` SET `position_z` = 44.57 WHERE `guid`=200956;
-
+UPDATE `creature` SET `position_z` = 44.57 WHERE `guid`=200949;
+UPDATE `creature` SET `position_z` = 44.57 WHERE `guid`=200956;
 -- Las dos siguientes, llegando a marrowgar
-UPDATE `creature_template` SET `position_z` = 42.1 WHERE `guid`=200912;
-UPDATE `creature_template` SET `position_z` = 42.1 WHERE `guid`=200934;
+UPDATE `creature` SET `position_z` = 42.1 WHERE `guid`=200912;
+UPDATE `creature` SET `position_z` = 42.1 WHERE `guid`=200934;
+
+-- Icc adds putricidio
+-- Ajuste de velocidades para babosas de putricidio, amenorada la velocidad
+UPDATE `creature_template` SET `speed_walk`=0.73, `speed_run`=0.72857 WHERE `entry` IN (37562,38602,38760,38761);
+UPDATE `creature_template` SET `speed_walk`=1.06, `speed_run`=0.72857 WHERE `entry` IN (37697,38604,38758,38759);
+
+-- Icc triggers de Rotface
+-- Eliminado algunos Puddle Stalker(37013) que hacen que salga en el aire los charcos
+DELETE FROM `creature` WHERE `guid`=201602;
+DELETE FROM `creature` WHERE `guid`=201332;
+DELETE FROM `creature` WHERE `guid`=201651;
+DELETE FROM `creature` WHERE `guid`=201494;
+DELETE FROM `creature` WHERE `guid`=201600;
+DELETE FROM `creature` WHERE `guid`=201648;
+DELETE FROM `creature` WHERE `guid`=201441;
+DELETE FROM `creature` WHERE `guid`=200922;
