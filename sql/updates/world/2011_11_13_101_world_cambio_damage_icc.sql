ea287d40d297041e09342bf632d4817f837171e7
Eilo <eilo2518@gmail.com>
2011-11-26 11:18:02 -0500

    ICC: Ajuste de daños fisicos para Rotface y Festergut, sobre todo festergut esta un poco alto

--- sql/updates/world/2011_11_13_101_world_cambio_damage_icc.sql
+++ sql/updates/world/2011_11_13_101_world_cambio_damage_icc.sql
@@ -6,10 +6,10 @@ UPDATE `creature_template` SET  `mindmg` =  '1509',`maxdmg` =  '2683',`dmg_multi
 -- Deathbringer
 UPDATE `creature_template` SET  `mindmg` =  '1509',`maxdmg` =  '2683',`dmg_multiplier` =  '38' WHERE `entry` =37813;-- Valor Anterior 35
 -- Rotface
-UPDATE `creature_template` SET  `mindmg` =  '1509',`maxdmg` =  '2683',`dmg_multiplier` =  '38' WHERE `entry` =36627;-- Valor Anterior 35
+UPDATE `creature_template` SET  `mindmg` =  '1509',`maxdmg` =  '2683',`dmg_multiplier` =  '30.5' WHERE `entry` =36627;-- Valor Anterior 35
 -- 36899 Bigg Ozze
 UPDATE `creature_template` SET  `mindmg` =  '1509',`maxdmg` =  '2683',`dmg_multiplier` =  '9.5' WHERE  `entry` =36899;-- Valor Anterior 7.5
 -- Festergut
-UPDATE `creature_template` SET  `mindmg` =  '1509',`maxdmg` =  '2683',`dmg_multiplier` =  '38' WHERE `entry` =36626;-- Valor Anterior 35
+UPDATE `creature_template` SET  `mindmg` =  '1509',`maxdmg` =  '2683',`dmg_multiplier` =  '28.5' WHERE `entry` =36626;-- Valor Anterior 35
 -- Sister Svalna
 UPDATE `creature_template` SET  `mindmg` =  '1509',`maxdmg` =  '2683',`dmg_multiplier` =  '10.5' WHERE  `entry` =37126;-- Valor Anterior 7.5
