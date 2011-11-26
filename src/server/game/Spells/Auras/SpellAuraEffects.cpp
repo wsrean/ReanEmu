f37aeb7f89d090f6fe251c4d0547f80464d6fcbf
Hispano <jesus.diaz.gutierrez@gmail.com>
2011-11-26 21:28:43 +0100

    3.6 Chamán: Escudos de Agua (52127)

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -592,7 +592,14 @@ int32 AuraEffect::CalculateAmount(Unit* caster)
                 break;
             // Earth Shield
             if (GetSpellInfo()->SpellFamilyName == SPELLFAMILY_SHAMAN && m_spellInfo->SpellFamilyFlags[1] & 0x400)
+            {
+               // return to unmodified by spellmods value
+               amount = m_spellInfo->Effects[m_effIndex].BasePoints;
+               // apply spell healing bonus
                 amount = caster->SpellHealingBonus(GetBase()->GetUnitOwner(), GetSpellInfo(), amount, SPELL_DIRECT_DAMAGE);
+               // apply spellmods
+               amount = caster->ApplyEffectModifiers(GetSpellInfo(), m_effIndex, float(amount));
+            }
             break;
         case SPELL_AURA_PERIODIC_DAMAGE:
             if (!caster)
