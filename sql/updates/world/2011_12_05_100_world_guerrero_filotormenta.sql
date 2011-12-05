2bf0a71cc4a207dd67547345483918fcbbf45bc5
Hispano <jesus.diaz.gutierrez@gmail.com>
2011-12-05 23:09:39 +0100

    Guerrero: filotormenta (46924): fix inmunidades y ahora no deberia retirar el aura al cambiar armas

---
+++ sql/updates/world/2011_12_05_100_world_guerrero_filotormenta.sql
@@ -0,0 +1,9 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 46924;
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
+(46924, -13810, 2, 'Bladestorm immune at Frost Trap'),
+(46924, -51514, 2, 'Bladestorm immune at Hex'),
+(46924, -116, 2, 'Bladestorm immune at FrostBolt'),
+(46924, -45524, 2, 'Bladestorm immune at Chains of Ice'),
+(46924, -68766, 2, 'Bladestorm immune at Desecration'),
+(46924, -58617, 2, 'Bladestorm immune at Glyph of Heart Strike'),
+(46924, -605, 2, 'Bladestorm immune at Mind Control');
