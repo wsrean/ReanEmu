b3ef85058e350b9d244f1b74caa76f61f9c8fa6d
Eilo <eilo2518@gmail.com>
2011-12-16 12:34:25 -0500

    2.2.29 ICC: LichKing Valkyrs ajuste inmunidades

--- sql/updates/world/reanemu/2011_11_24_100_world_correcciones_icc.sql
+++ sql/updates/world/reanemu/2011_11_24_100_world_correcciones_icc.sql
@@ -33,3 +33,7 @@ DELETE FROM `creature` WHERE `guid`=201600;
 DELETE FROM `creature` WHERE `guid`=201648;
 DELETE FROM `creature` WHERE `guid`=201441;
 DELETE FROM `creature` WHERE `guid`=200922;
+
+-- Icc adds Lich King
+-- Valkyrs de lich king(36609)
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|1|2|4|8|16|32|64|128|256|512|4096|8192|65536|131072|524288|1048576|4194304|8388608|33554432|67108864|536870912 WHERE `entry` IN (36609,39120,39121,39122);
