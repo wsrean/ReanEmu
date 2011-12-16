01c28eb7c92a0170a6a184770d120b1f9d467068
Eilo <eilo2518@gmail.com>
2011-12-16 18:06:58 -0500

    2.10.1 Ahn'Kahet: Taldaram combate en invisible

--- src/server/scripts/Northrend/AzjolNerub/Ahnkahet/boss_prince_taldaram.cpp
+++ src/server/scripts/Northrend/AzjolNerub/Ahnkahet/boss_prince_taldaram.cpp
@@ -226,6 +226,7 @@ public:
                             {
                                 DoScriptText(RAND(SAY_VANISH_1, SAY_VANISH_2), me);
                                 DoCast(me, SPELL_VANISH);
+                                me->SetInCombatWithZone();
                                 Phase = JUST_VANISHED;
                                 uiPhaseTimer = 500;
                                 if (Unit* pEmbraceTarget = SelectTarget(SELECT_TARGET_RANDOM, 0, 100, true))
