f721d0275c375a0bf820c35b06d9caef7ab8645f
Eilo <eilo2518@gmail.com>
2012-01-03 18:11:08 -0500

    5.2 Nombres a game_tele de icc y test

---
+++ sql/updates/world/traducciones/game_tele_rean.sql
@@ -0,0 +1,5 @@
+-- Cambio de nombre de 2 sitios que se usa mucho por el staff
+-- Eilo (https://github.com/eilo)
+
+UPDATE game_tele SET NAME='test' WHERE id=817;
+UPDATE game_tele SET NAME='icc' WHERE id=1418;
