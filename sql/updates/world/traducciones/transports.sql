ab041fc529dc024074d87473a4637ca415c90d7f
Eilo <eilo2518@gmail.com>
2012-01-03 18:08:30 -0500

    5.1 Traducciones de eswow2 rev1 tlk

---
+++ sql/updates/world/traducciones/transports.sql
@@ -0,0 +1,35 @@
+-- ============== --
+--   TRANSPORTS   --
+-- ============== --
+
+ALTER TABLE `transports` CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+UPDATE `transports` SET `name`='Campamento Grom\'gol y Entrañas' WHERE `entry`='176495';
+UPDATE `transports` SET `name`='Puerto de Menethil y Auberdine' WHERE `entry`='176310';
+UPDATE `transports` SET `name`='Teldrassil y Auberdine' WHERE `entry`='176244';
+UPDATE `transports` SET `name`='Puerto de Menethil y Theramore' WHERE `entry`='176231';
+UPDATE `transports` SET `name`='Campamento Grom\'gol y Orgrimmar' WHERE `entry`='175080';
+UPDATE `transports` SET `name`='Orgrimmar y Entrañas' WHERE `entry`='164871';
+UPDATE `transports` SET `name`='Trinquete y Bahía del Botín' WHERE `entry`='20808';
+UPDATE `transports` SET `name`='La Costa Olvidada y Bastión Plumaluna' WHERE `entry`='177233';
+UPDATE `transports` SET `name`='Isla Bruma Azur y Auberdine' WHERE `entry`='181646';
+UPDATE `transports` SET `name`='Puerto de Ventormenta y Fortaleza Denuedo, Tundra Boreal (\"El Kraken\")' WHERE `entry`='190536';
+UPDATE `transports` SET `name`='Valgarde y Menethil' WHERE `entry`='181688';
+UPDATE `transports` SET `name`='Entrañas y Campo Venganza' WHERE `entry`='181689';
+UPDATE `transports` SET `name`='Orgrimmar y Bastión Grito de Guerra' WHERE `entry`='186238';
+UPDATE `transports` SET `name`='Zeppelin robado' WHERE `entry`='186371';
+UPDATE `transports` SET `name`='Barco tortuga del Puerto Moa\'ki' WHERE `entry`='187568';
+UPDATE `transports` SET `name`='Barco pirata' WHERE `entry`='187038';
+UPDATE `transports` SET `name`='Barco tortuga de Unu\'pe' WHERE `entry`='188511';
+UPDATE `transports` SET `name`='El Martillo de Orgrim' WHERE `entry`='192241';
+UPDATE `transports` SET `name`='Pista de Aterrizaje de Palanqueta' WHERE `entry`='192242';
+UPDATE `transports` SET `name`='Orgrimmar y Cima del Trueno' WHERE `entry`='190549';
+UPDATE `transports` SET `name`='Ciudadela de Corona de Hielo_Horda_10' WHERE `entry`='201812';
+UPDATE `transports` SET `name`='El Martillo de Orgrim' WHERE `entry`='201599';
+UPDATE `transports` SET `name`='Banda de Ciudadela de Corona de Hielo, El Martillo de Orgrim' WHERE `entry`='201581';
+UPDATE `transports` SET `name`='El Rompecielos' WHERE `entry`='201598';
+UPDATE `transports` SET `name`='Banda de Ciudadela de Corona de Hielo, El Rompecielos' WHERE `entry`='201580';
+UPDATE `transports` SET `name`='Ciudadela de Corona de Hielo_Alianza_10' WHERE `entry`='201811';
+UPDATE `transports` SET `name`='Nave de guerra de la Horda' WHERE `entry`='195276';
+UPDATE `transports` SET `name`='Nave de guerra de la Alianza' WHERE `entry`='195121';
+UPDATE `transports` SET `name`='Zeppelin, Horda (El Viento Poderoso) (Banda de Corona de Hielo)' WHERE `entry`='201834';
