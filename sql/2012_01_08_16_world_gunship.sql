65f952585338710f4becd605d7d125c55e31eaf8
Eilo <eilo2518@gmail.com>
2012-02-01 15:40:38 -0500

    Gunship: Añadidos al sql y pruebas en 10/25

--- sql/2012_01_08_16_world_gunship.sql
+++ sql/2012_01_08_16_world_gunship.sql
@@ -4,21 +4,24 @@
 
 -- Videos adicionales http://www.youtube.com/watch?v=h36xHUdXm84
 -- Gunship Battle
+
+-- Muradin y Saurfang
 DELETE FROM `creature_template` WHERE (`entry`=50004);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction_A`, `faction_H`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `Health_mod`, `Mana_mod`, `Armor_mod`, `RacialLeader`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `movementId`, `RegenHealth`, `equipment_id`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `WDBVerified`) VALUES
 (50004, 0, 0, 0, 0, 0, 11686, 0, 0, 0, 'High Overlord Saurfang', '', '', 0, 80, 80, 3, 1802, 1802, 0, 0, 0, 1, 1, 252, 357, 0, 304, '7.5', 2000, 0, 1, 33554432, 8, 0, 0, 0, 0, 0, 215, 320, 44, 7, 72, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1000, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, '', 12340);
-
 DELETE FROM `creature_template` WHERE (`entry`=50006);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction_A`, `faction_H`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `Health_mod`, `Mana_mod`, `Armor_mod`, `RacialLeader`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `movementId`, `RegenHealth`, `equipment_id`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `WDBVerified`) VALUES
 (50006, 0, 0, 0, 0, 0, 11686, 0, 0, 0, 'Muradin Bronzebeard', '', '', 10875, 83, 83, 2, 1802, 1802, 1, 1, '1.14286', 1, 1, 468, 702, 0, 175, '47.2', 2000, 2000, 1, 33554432, 8, 0, 0, 0, 0, 0, 374, 562, 140, 7, 76, 0, 0, 0, 0, 0, 0, 0, 0, 0, 15284, 70309, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 175, 1, 1, 0, 0, 0, 0, 0, 0, 0, 164, 1, 0, 0, 0, '', 12340);
 UPDATE `creature_template` SET `flags_extra` = 2 WHERE `entry` IN (50004, 50006);
 
+-- Varios
 UPDATE `creature_template` SET `npcflag` = 1 WHERE `entry` IN (36939, 38156, 38637, 38638);
 
+-- Templates del trash
 UPDATE `creature_template` SET `faction_A` = 1801, `faction_H` = 1801, `baseattacktime` = 2000, `rangeattacktime` = 2000, `unit_flags` = 32832, `spell1` = 71339, `spell2` = 70161, `spell3` = 72539, `mechanic_immune_mask` = 73745 WHERE `entry` IN (36968, 38403, 38675, 38676);
 UPDATE `creature_template` SET `faction_A` = 1802, `faction_H` = 1802, `baseattacktime` = 2000, `rangeattacktime` = 2000, `unit_flags` = 32832, `spell1` = 71335, `spell2` = 70162, `spell3` = 72566, `mechanic_immune_mask` = 73745 WHERE `entry` IN (36969, 38408, 38689, 38690);
 
--- Spawn fix for TDB GUIDs
+-- Spawn para el sorcerer y el vindicator
 DELETE FROM `creature` WHERE `id`=37003;
 INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`) VALUES
 (200988, 37003, 631, 15, 1, 0, 0, -562.385, 2217.58, 199.969, 3.46228, 86400, 0, 0, 398434, 41690, 0, 0, 0, 0);
@@ -26,6 +29,7 @@ DELETE FROM `creature` WHERE `id`=37026;
 INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`) VALUES
 (201020, 37026, 631, 15, 1, 0, 0, -571.091, 2219.71, 199.969, 5.06842, 86400, 0, 0, 269600, 91600, 0, 0, 0, 0);
 
+-- Scriptnames +1
 UPDATE `creature_template` SET `ScriptName` = 'npc_muradin_gunship' WHERE `entry` = 36948;
 UPDATE `creature_template` SET `ScriptName` = 'npc_saurfang_gunship' WHERE `entry` = 36939;
 UPDATE `creature_template` SET `ScriptName` = 'npc_zafod_boombox' WHERE `entry` = 37184;
@@ -53,13 +57,13 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_gunship_cannon' WHERE `entry`
 
 UPDATE `gameobject_template` SET `ScriptName` = 'transport_gunship' WHERE `entry` IN (201580, 201812);
 
-
+-- Areatriggers <--- OJO AQUI para los datas de frostwyrms
 DELETE FROM `areatrigger_scripts` WHERE `entry` IN (5630, 5628);
 INSERT INTO `areatrigger_scripts` (`entry`,`ScriptName`) VALUES
 (5628,'at_icc_land_frostwyrm'),
 (5630,'at_icc_land_frostwyrm');
 
--- SpellScripts
+-- SpellScripts para la batalla <---- aqui hay que apañar las scripts para añadir explosiones
 DELETE FROM `spell_script_names` WHERE `spell_id` IN (70713, 68645, 69471, 69488, 69402, 70175, 71335, 71339);
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 ('70713', 'spell_icc_remove_rocket_pack'),
@@ -71,6 +75,7 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 ('71335', 'spell_gb_burning_pitch'),
 ('71339', 'spell_gb_burning_pitch');
 
+-- Evitando posibles cosillas :)
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceEntry` IN (69400, 70173, 69402, 70175, 70374, 70383, 72959);
 SET @ENTRYA := 37540;
 SET @ENTRYH := 37215;
@@ -83,25 +88,30 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceEntry`, `ConditionTy
 ('13', '70383', '18', '1', @ENTRYH),
 ('13', '72959', '18', '1', 0);
 
+-- Hacky, hay que darle nuevos parametros a los transportes de aqui
 DELETE FROM transports WHERE entry IN (201580, 201581, 201811, 201812);
 DELETE FROM creature WHERE id IN (37230, 36838, 36839, 36948, 36939);
 DELETE FROM `creature_template_addon` WHERE (`entry`=37547);
 UPDATE `creature_template` SET `flags_extra` = 130 WHERE `entry` = 37547;
 
+-- Para los cañones
 UPDATE `creature_template` SET `exp` = 2, `VehicleId` = 554, `RegenHealth` = 0, `npcflag` = 16777216, `spell1` = 69399, `spell2` = 69401 WHERE entry = 36839;
 UPDATE `creature_template` SET `exp` = 2, `VehicleId` = 554, `RegenHealth` = 0, `npcflag` = 16777216, `spell1` = 70172, `spell2` = 70174 WHERE entry = 36838;
 REPLACE INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `quest_start`, `cast_flags`) VALUES
 ('36838', '46598', '0', '1'),
 ('36839', '46598', '0', '1');
-
 DELETE FROM `creature_template_addon` WHERE `entry` IN (36838, 36839);
 INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
 (36838, 0, 0, 0, 1, 0, 69470),
 (36839, 0, 0, 0, 1, 0, 69470);
-
 UPDATE `creature_template` SET `difficulty_entry_1` = '38129',`difficulty_entry_2` = '38701', `difficulty_entry_3` = '38702' WHERE `entry` =37215;
 UPDATE `creature_template` SET `faction_A` = 35, `faction_H` = 35, `minlevel` = 83, `maxlevel` = 83, `unit_flags` = 4, `flags_extra` = 2, `modelid1` = 1126, `modelid2` = 11686 WHERE `entry` IN (37215, 38129, 38701, 38702, 37540, 38128, 38699, 38700);
+-- Arreglando los cañones en 25man
+UPDATE `creature_template` SET `difficulty_entry_1`=0 WHERE `entry`=36839 LIMIT 1;
+-- Evitando que se muevan duh
+UPDATE `creature_template` SET `speed_walk`=0 AND `speed_run`=0 WHERE `entry`=36839 AND `entry`=36838;
 
+-- Mas condiciones
 -- Add spell conditions for 69705 (Below Zero)
 SET @SPELL := 69705;
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceEntry`=@SPELL;
@@ -109,9 +119,7 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,
 (13,0,@SPELL,0,18,1,36838,0,0,'','Gunship Battle - Spell 69705 (Below Zero) target creature 36838'),
 (13,0,@SPELL,0,18,1,36839,0,0,'','Gunship Battle - Spell 69705 (Below Zero) target creature 36839');
 
-
--- Linked spells
--- TODO Need more love
+-- AQUI HAY QUE AÑADIR LOS EFECTOS DE EXPLOSION ***** OJO ******
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 71193 AND `spell_effect` = -71188;
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 71195 AND `spell_effect` = -71193;
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 68645 AND `spell_effect` = 69193;
@@ -125,7 +133,7 @@ INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comm
 (68645, 69193, 0, 'Gunship Battle - Fix visual for Rocket Pack'),
 (69193, 69192, 0, 'Gunship Battle - Cast Rocket Burst when landing with Rocket Pack');
 
--- Battle Standards Spells
+-- Spells <---------- Aqui hay que fijarse bien que el trash este ejecutando las cosas como debe
 UPDATE `creature_template` SET `unit_flags` = 33554564, `flags_extra` = 2 WHERE `entry` IN (37044, 37041);
 UPDATE `creature_template` SET `faction_A` = 1801, `faction_H` = 1801 WHERE `entry` = 37044;
 UPDATE `creature_template` SET `faction_A` = 1802, `faction_H` = 1802 WHERE `entry` = 37041;
@@ -140,14 +148,14 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@KORKRON ,0,0,0,25,0,100,1,0,0,0,0,11,@HSPELL,2,0,0,0,0,1,0,0,0,0,0,0,0, 'Kor''kron Battle Standard - On spawn & reset - Cast spell 69809'),
 (@SKYBREAKER,0,0,0,25,0,100,1,0,0,0,0,11,@ASPELL,2,0,0,0,0,1,0,0,0,0,0,0,0, 'Skybreaker Battle Standard - On spawn & reset - Cast spell 69808');
 
-
+-- Mas textos
 REPLACE INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`) VALUES
 (50004, 1, 0, 'Reavers, Sergeants, attack!.', 14, 0, 0, 0, 0, 17081, ''),
 (50004, 0, 0, 'This is not your battle, dwarf. Back down or we will be forced to destroy your ship.', 14, 0, 0, 0, 0, 17093, ''),
 (50006, 1, 0, 'Marines! Sergeants! Attack!', 14, 0, 0, 0, 0, 16956, ''),
 (50006, 0, 0, 'Move yer jalopy or well blow it out of the sky, orc! The Hordes got no business here!', 14, 0, 0, 0, 0, 16969, '');
 
--- Texts
+-- Textos
 SET @MURADIN := 36948;
 SET @OVERLORD := 36939;
 SET @PRIMALIST := 37030;
@@ -204,7 +212,7 @@ INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`pr
 (@FROSTWYRM,0, 0, 'A Spire Frostwyrm lands just before Orgrim''s Hammer.', 16, 0, 100, 0, 0, @SOUNDID, 'Frostwyrm - SAY_FROSTWYRM_LAND_H_1'),
 (@FROSTWYRM,1, 0, 'A Spire Frostwyrm lands just before The Skybreaker. ', 16, 0, 100, 0, 0, @SOUNDID, 'Frostwyrm - SAY_FROSTWYRM_LAND_A_2');
 
--- Chests loot
+-- Loot de los cofres :)
 DELETE FROM `gameobject_loot_template` WHERE (`entry`=201872);
 INSERT INTO `gameobject_loot_template` VALUES
 (201872, 49426, 100, 1, 0, 2, 2),
@@ -280,6 +288,7 @@ INSERT INTO `gameobject_loot_template` VALUES
 (201875, 50665, 0, 1, 1, 1, 1),
 (201875, 50667, 0, 1, 3, 1, 1);
 
+-- Esto es un poco hacky, para los transportes
 UPDATE `gameobject_template` SET `data1` = 201872 WHERE `entry` = 201872;
 UPDATE `gameobject_template` SET `data1` = 201873 WHERE `entry` = 201873;
 UPDATE `gameobject_template` SET `data1` = 201874 WHERE `entry` = 201874;
@@ -289,27 +298,9 @@ UPDATE `gameobject_template` SET `data1` = 201873 WHERE `entry` = 202178;
 UPDATE `gameobject_template` SET `data1` = 201874 WHERE `entry` = 202179;
 UPDATE `gameobject_template` SET `data1` = 201875 WHERE `entry` = 202180;
 
+-- El rocket pack
 UPDATE item_template SET ScriptName = 'item_icc_rocket_pack' WHERE entry = 49278;
 
 -- Zafod Boombox (37184) giver of jetpack
 UPDATE `creature_template` SET `npcflag`=1 WHERE `entry`=37184;
 
--- Gunship cannons
-DELETE FROM `npc_spellclick_spells` WHERE `npc_entry` IN (36838,36839);
-INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `quest_start`, `quest_start_active`, `quest_end`, `cast_flags`, `aura_required`, `aura_forbidden`, `user_type`) VALUES
-(36838,46598,0,0,0,1,0,0,0),
-(36839,46598,0,0,0,1,0,0,0);
-DELETE FROM `creature_template` WHERE `entry` IN (36838,36839);
-INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction_A`, `faction_H`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `Health_mod`, `Mana_mod`, `Armor_mod`, `RacialLeader`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `movementId`, `RegenHealth`, `equipment_id`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `WDBVerified`) VALUES
-(36838,35410,0,0,0,0,29488,0,0,0,'Alliance Gunship Cannon','','vehichleCursor',0,80,80,0,1732,1732,0,0,0,1,0,422,586,0,642,1,0,0,1,0,16384,0,0,0,0,0,345,509,103,9,8,0,0,0,0,0,0,0,0,0,70172,70174,0,0,0,0,0,0,0,554,0,0,'SmartAI',0,3,65.5185,1,1,0,0,0,0,0,0,0,0,1,0,0,0,'',12340),
-(36839,35427,0,0,0,0,29489,0,0,0,'Horde Gunship Cannon','','vehichleCursor',0,80,80,0,1665,1665,0,0,0,1,0,422,586,0,642,1,0,0,1,16384,8,0,0,0,0,0,345,509,103,9,8,0,0,0,0,0,0,0,0,0,69399,69401,0,0,0,0,0,0,0,0,0,0,'SmartAI',0,3,65.5185,1,1,0,0,0,0,0,0,0,0,1,0,0,0,'',12340);
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (36838,36839);
-INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(36838,0,0,0,25,0,100,0,0,0,0,0,75,69470,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Heat Drain on reset'),
-(36839,0,0,0,25,0,100,0,0,0,0,0,75,69470,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Heat Drain on reset');
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` AND `SourceEntry` IN (69400,69402,70173,70715);
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(13,0,69400,0,18,1,37540,0,0, '', NULL), -- Horde Cannon (Cannon Blast) set to hit only Skybreaker (effect 2)
-(13,0,69402,0,18,1,37540,0,0, '', NULL), -- Horde Cannon (Incinerating Blast) set to hit only Skybreaker (effect 2)
-(13,0,70173,0,18,1,37215,0,0, '', NULL), -- Alliance Cannon (Cannon Blast) set to hit only Orgrimms Hammer (effect 2)
-(13,0,70175,0,18,1,37215,0,0, '', NULL); -- Alliance Cannon (Incinerating Blast) set to hit only Orgrimms Hammer (effect 2)
